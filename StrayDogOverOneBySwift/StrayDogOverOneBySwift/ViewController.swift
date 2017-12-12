16110335aceaeff70d65fe01212b475a2417413e
平山亮 <Hirarin@hirayamaryou-no-MacBook-Air.local>
2017-12-13 08:50:26 +0900

    [機能]新規プロジェクト作成

---
+++ StrayDogOverOneBySwift/StrayDogOverOneBySwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrayDogOverOneBySwift
+//
+//  Created by 平山亮 on 2017/07/23.
+//  Copyright © 2017年 平山亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
